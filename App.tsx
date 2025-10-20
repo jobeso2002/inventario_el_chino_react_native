fb1d7430fc719756071aa68b80350372d9532018
jobeso2002 <jobeso@unicesar.edu.co>
2025-10-20 11:16:05 -0500

    fuentes para texto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Spinner } from '@core'
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+  // Use `useFonts` only if you can't use the config plugin.
+  const [loaded, error] = useFonts({
+    'PlaypenSans-Bold': require('./assets/fonts/PlaypenSans-Bold.ttf'),
+    'PlaypenSans-Light': require('./assets/fonts/PlaypenSans-Light.ttf'),
+    'PlaypenSans-Regular': require('./assets/fonts/PlaypenSans-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>iniciemos el inventario12!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: 'PlaypenSans-Bold', fontSize: 24, textAlign: 'center' }}>iniciemos el inventario123423478!</Text>
+      <Text style={{ fontFamily: 'PlaypenSans-Regular', fontSize: 24, textAlign: 'center' }}>iniciemos el inventario123423478!</Text>
+      <Text style={{ fontFamily: 'PlaypenSans-Light', fontSize: 24, textAlign: 'center' }}>iniciemos el inventario123423478!</Text>
+      
+      <StatusBar style='auto' />
+      <Spinner />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +42,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
